ff31afc887ca905fe2a9efbd9e5e1a7505cc77e1
Johan Steven Arias V <johan@johan.(none)>
2014-09-18 00:45:24 -0500

    Commented STATICFILES:DIRS

--- glosario/settings.py
+++ glosario/settings.py
@@ -92,9 +92,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 # Heroku settings
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
